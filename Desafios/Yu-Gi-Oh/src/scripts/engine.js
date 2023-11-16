c7437e26701afbe77886cd84ac73b96f9e5a6722
Filipe Lacerda <gdfkaiser@gmail.com>
2023-11-16 01:59:10 -0300

    Yu-Gi-Oh V.01

---
+++ Desafios/Yu-Gi-Oh/src/scripts/engine.js
@@ -0,0 +1,69 @@
+const state = {
+    score: {
+        playerScore: 0,
+        computerScore: 0,
+        scoreBox: document.getElementById("score_points"),
+    },
+    cardSprites: {
+        avatar: document.getElementById("card-image"),
+        name: document.getElementById("card-name"),
+        type: document.getElementById("card-type"),
+    },
+    fieldCards: {
+        player: document.getElementById("player-field-card"),
+        computer: document.getElementById("computer-field-card"),
+    },
+    actions: {
+        button: document.getElementById("next-duel"),
+    },
+};
+
+const playersSides = {
+    player1: "player-field-cards",
+    computer: "computer-field-cards",
+};
+
+const pathImages = ".src/assets/icons/";
+
+const cardData = [
+    {
+        id: 0,
+        name: "Blue Eyes White Dragon",
+        type: "Paper",
+        img: `${pathImages}dragon.png`,
+        winOf:[1],
+        loseOf: [2],
+    },
+    {
+        id: 1,
+        name: "Dark Magician",
+        type: "Rock",
+        img: `${pathImages}magician.png`,
+        winOf:[2],
+        loseOf: [0],
+    },
+    {
+        id: 2,
+        name: "Exodia",
+        type: "Scissors",
+        img: `${pathImages}exodia.png`,
+        winOf:[0],
+        loseOf: [1],
+    },
+];
+
+async function drawCards(cardNumbers, fieldSide) {
+    for (let i = 0; i < cardNumbers; i++) {
+        const randomIdCard = await getRandomCard();
+        const cardImage = await createCardImage(randomIdCard, fieldSide);
+
+        document.getElementById(fieldSide).appendChild(cardImage);
+    }
+}
+
+function init() {
+    drawCards(5, playersSides.player1)
+    drawCards(5, playersSides.computer)
+}
+
+init ();
